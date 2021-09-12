3733868f36457696270b9167b26cd755794ed4a1
shermzlim <shermz.lim.jh@gmail.com>
2021-09-12 20:10:14 +0800

    add sign in route to get jwt token

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -1,5 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
+const axios = require('axios').default;
+const admin = require("../firebase/firebase-config");
+const FIREBASE_API_KEY = process.env.FIREBASE_API_KEY;
+
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
@@ -8,6 +12,27 @@ class IndexController {
       next(error);
     }
   };
+
+  public signIn = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      const auth_response = await axios({
+        url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${FIREBASE_API_KEY}`,
+        method: 'post',
+        data: {
+          email: req.body.email,
+          password: req.body.password,
+          returnSecureToken: true
+        },
+        json: true,
+      });    
+
+      res.json({
+        "jwt": auth_response.data.idToken
+      });
+    } catch (err) {
+      res.sendStatus(400);
+    }
+  }
 }
 
 export default IndexController;
