195a0ee2cc0e2271f54102eedfa1f265a92fecaf
Sun Yitao <sunyitao7@gmail.com>
2021-09-13 02:54:06 +0800

    Add cors origin, fix eslint errors

--- src/firebase/firebase-config.ts
+++ src/firebase/firebase-config.ts
@@ -1,11 +1,11 @@
-var admin = require("firebase-admin");
+const admin = require('firebase-admin');
 
 // a firebasePrivateKey.json file should be added in this directory.
 // it can be obtained from Firebase Project Settings -> Service Accounts
-var serviceAccount = require("./firebasePrivateKey.json");
+const serviceAccount = require('./firebasePrivateKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
