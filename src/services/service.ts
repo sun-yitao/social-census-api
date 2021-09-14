1a0894ae2fcaeccaeca81882a16c1eb65a6ed892
Sun Yitao <sunyitao7@gmail.com>
2021-09-14 17:30:21 +0800

    Add more methods in Service

--- src/services/service.ts
+++ src/services/service.ts
@@ -4,16 +4,22 @@ import { isEmpty } from '@utils/util';
 class Service {
   public resource;
 
-  public async findAll<Type>(): Promise<Type[]> {
-    const all: Type[] = await this.resource.findMany();
+  public async findMany<Type>(options?: object): Promise<Type[]> {
+    const all: Type[] = await this.resource.findMany(options);
     return all;
+  }
+
+  public async findUnique<Type>(options?: object): Promise<Type> {
+    const one: Type = await this.resource.findUnique(options);
+    if (!one) throw new HttpException(404, `Resource not found`);
+    return one;
   }
 
   public async findById<Type>(id: number): Promise<Type> {
     if (isEmpty(id)) throw new HttpException(400, 'Invalid id');
 
     const one: Type = await this.resource.findUnique({ where: { id: id } });
-    if (!one) throw new HttpException(409, `Id ${id} not found`);
+    if (!one) throw new HttpException(404, `Id ${id} not found`);
 
     return one;
   }
@@ -24,11 +30,17 @@ class Service {
     return createData;
   }
 
+  public async createMany<Type>(options?: object): Promise<Type[]> {
+    if (isEmpty(options)) throw new HttpException(400, 'Empty data received');
+    const createData: Type[] = await this.resource.create(options);
+    return createData;
+  }
+
   public async update<Type>(id: number, data: object): Promise<Type> {
     if (isEmpty(data)) throw new HttpException(400, 'Empty data received');
 
     const one: Type = await this.resource.findUnique({ where: { id: id } });
-    if (!one) throw new HttpException(409, `Id ${id} not found`);
+    if (!one) throw new HttpException(404, `Id ${id} not found`);
 
     const updateData = await this.resource.update({ where: { id: id }, data: data });
     return updateData;
@@ -38,7 +50,7 @@ class Service {
     if (isEmpty(id)) throw new HttpException(400, 'Invalid id');
 
     const one: Type = await this.resource.findUnique({ where: { id: id } });
-    if (!one) throw new HttpException(409, `Id ${id} not found`);
+    if (!one) throw new HttpException(404, `Id ${id} not found`);
 
     const deleteData = await this.resource.delete({ where: { id: id } });
     return deleteData;
