efc0b79484824204ee52046e6dab6f6d7546690a
shermzlim <shermz.lim.jh@gmail.com>
2021-09-14 17:24:08 +0800

    change responses route to return response created

--- src/controllers/response.controller.ts
+++ src/controllers/response.controller.ts
@@ -1,14 +1,21 @@
 import { NextFunction, Response } from 'express';
 import { RequestWithUser } from '@/interfaces/auth.interface';
 import prismaClient from '@/prisma/client';
+import { HttpException } from '@/exceptions/HttpException';
 
 class ResponseController {
   public create = async (req: RequestWithUser, res: Response, next: NextFunction): Promise<void> => {
     try {
       const userId = req.user.uid;
+      const responses = req.body.responses;
+      // if responses empty or not all responses has same questionId
+      if (responses.length == 0 || !responses.every((val, i, arr) => val.questionId === arr[0].questionId)) {
+        throw new HttpException(400, 'Invalid responses');
+      }
+      const questionId = responses[0].questionId;
 
-      const responsesCount = await prismaClient.response.createMany({
-        data: req.body.responses.map(r => {
+      await prismaClient.response.createMany({
+        data: responses.map(r => {
           return {
             uid: userId,
             optionId: r.optionId,
@@ -17,7 +24,15 @@ class ResponseController {
         }),
       });
 
-      res.json(responsesCount);
+      const userQuestionResponses = await prismaClient.response.findMany({
+        where: {
+          uid: userId,
+          questionId: questionId,
+        },
+      });
+
+      res.status(201);
+      res.json(userQuestionResponses);
     } catch (error) {
       next(error);
     }
